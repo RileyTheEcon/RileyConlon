e15b8db749323a900a81b87b89c425f2576f6687
RileyTheEcon <56739608+RileyTheEcon@users.noreply.github.com>
2019-12-15 13:32:19 -0500

    Add files via upload

---
+++ contact-me.php
@@ -0,0 +1,14 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone']:
+$message = $_POST['message'];
+
+$recipient = "r.conlon9494@gmail.com";
+$subject = "Contact Response from $name";
+$formcontent = "Message: $message \n $name \n $email \n $phone"
+$mailheader = "From: $email & $phone";
+
+mail($recipient,$subject,$formcontent,$mailheader);
+echo "Thank you!"
+?>
